ac47b5e039e29be646f59859e6144c17d36511e1
Joana Hahne <joanahahne@yahoo.de>
2021-09-04 11:14:20 +0200

    Version1

---
+++ GUI.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+window = tk.Tk()
+lsolltemp = tk.Label(text="Soll-Temperatur")
+lsolltemp.pack()
+listtemp = tk.Label(text="Ist-Temperatur")
+listtemp.pack()
+window.mainloop()
